25d97c249b20ca3e6222452d89924c368bc2bd3b
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-07-13 14:17:42 +0200

    Created search function

--- src/utils/searchfunction.jsx
+++ src/utils/searchfunction.jsx
@@ -1,7 +1,13 @@
-export function selectPodcastByTitle(podcasts, title) {
-  return (
-    podcasts.find(
-      (podcast) => podcast.title.toLowerCase() === title.toLowerCase()
-    ) || null
+/**
+ * Filters podcasts by title starting with the search string.
+ * @param {Array} podcasts - Array of podcast objects with a 'title' property.
+ * @param {string} search - The search string.
+ * @returns {Array} Filtered podcasts.
+ */
+export function searchPodcastsByTitleStart(podcasts, search) {
+  if (!search) return podcasts;
+  const lowerSearch = search.toLowerCase();
+  return podcasts.filter((podcast) =>
+    podcast.title.toLowerCase().startsWith(lowerSearch)
   );
 }
