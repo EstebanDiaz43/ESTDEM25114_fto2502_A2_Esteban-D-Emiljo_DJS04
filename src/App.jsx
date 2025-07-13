fc1949ea9eaac9d4bda2abe17d3d9de831161815
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-07-13 05:22:30 +0200

    Added filter section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import PodcastGrid from "./components/PodcastGrid";
 import { genres } from "./data";
 import { fetchPodcasts } from "./api/fetchPodcasts";
 import Header from "./components/Header";
+import Filtersection from "./components/Filter";
 
 /**
  * App - The root component of the Podcast Explorer application. It handles:
@@ -24,6 +25,7 @@ export default function App() {
   return (
     <>
       <Header />
+      <Filtersection />
       <main>
         {loading && (
           <div className="message-container">
---
+++ src/components/Filter.jsx
@@ -0,0 +1,7 @@
+export default function Filtersection() {
+  return (
+    <filtersection className="filter-header">
+      <h3>Filter by:</h3>
+    </filtersection>
+  );
+}
